08893eb79c16126d6663f646629ddf4fd0c0080b
Tim Qi <i@timqi.com>
2016-05-07 22:14:52 +0800

    first commit

---
+++ library/src/main/java/com/timqi/windrosediagram/WindRoseDiagramView.java
@@ -0,0 +1,22 @@
+package com.timqi.windrosediagram;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by qiqi on 16/5/7.
+ */
+public class WindRoseDiagramView extends View {
+    public WindRoseDiagramView(Context context) {
+        super(context);
+    }
+
+    public WindRoseDiagramView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WindRoseDiagramView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
