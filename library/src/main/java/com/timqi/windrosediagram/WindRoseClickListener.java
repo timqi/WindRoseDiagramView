86e70e6809b2f1f5b57dff8d7a8ebac46985cd1f
Tim Qi <i@timqi.com>
2016-05-10 11:39:22 +0800

    c1

--- library/src/main/java/com/timqi/windrosediagram/WindRoseClickListener.java
+++ library/src/main/java/com/timqi/windrosediagram/WindRoseClickListener.java
@@ -1,8 +1,5 @@
 package com.timqi.windrosediagram;
 
-/**
- * Created by qiqi on 16/5/9.
- */
 public interface WindRoseClickListener {
 
   void onItemClick(int position);
--- library/src/main/java/com/timqi/windrosediagram/WindRoseDiagramView.java
+++ library/src/main/java/com/timqi/windrosediagram/WindRoseDiagramView.java
@@ -4,15 +4,13 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Point;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-/**
- * Created by qiqi on 16/5/7.
- */
 public class WindRoseDiagramView extends View {
 
   private Context mCtx;
@@ -22,11 +20,11 @@ public class WindRoseDiagramView extends View {
 
   private RectF mOvalRectF;
 
-  private int mOvalBackgroundColor = 0xff00ff00;
+  private int mOvalBackgroundColor = 0;
 
   private Paint mOutlinePaint;
 
-  private int mOutlineColor = 0xffff5534;
+  private int mOutlineColor = 0xccffffff;
 
   private int mOutlineWidth;
 
@@ -37,14 +35,22 @@ public class WindRoseDiagramView extends View {
   private float mStartAngle = 0;
 
   // for foreground
-  private int mForegroundColor = 0xff0000ff;
+  private Paint mForegroundPaint;
+
+  private int mForegroundColor = 0x881888ec;
+
+  private Paint mAnchorPaint;
+
+  private int mAnchorWidth;
+
+  private int mAnchorColor = 0xffffffff;
 
   // for text
   private Paint mTextPaint;
 
   private int mTextSize;
 
-  private int mTextColor = 0xffffffff;
+  private int mTextColor = 0xccffffff;
 
   // data
   private WindRoseDiagramAdapter mAdapter;
@@ -60,6 +66,8 @@ public class WindRoseDiagramView extends View {
   // widget
   private WindRoseClickListener windRoseClickListener;
 
+  private int mTouchSlop;
+
   public void setWindRoseClickListener(WindRoseClickListener listener) {
     this.windRoseClickListener = listener;
   }
@@ -67,82 +75,129 @@ public class WindRoseDiagramView extends View {
 
   public WindRoseDiagramView(Context context) {
     super(context);
-    this.mCtx = context;
-
-    init();
+    init(context);
   }
 
   public WindRoseDiagramView(Context context, AttributeSet attrs) {
     super(context, attrs);
-    this.mCtx = context;
+    init(context);
 
     TypedArray attributes = context.getTheme()
         .obtainStyledAttributes(attrs, R.styleable.WindRoseDiagramView, 0, 0);
     initAttrs(attributes);
-
-    init();
   }
 
   public WindRoseDiagramView(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
-    this.mCtx = context;
+    init(context);
 
     TypedArray attributes = context.getTheme()
         .obtainStyledAttributes(attrs, R.styleable.WindRoseDiagramView, defStyleAttr, 0);
     initAttrs(attributes);
-
-    init();
   }
 
   private void initAttrs(TypedArray attributes) {
 
     try {
-      mTextColor = attributes.getColor(R.styleable.WindRoseDiagramView_textColor, 0xffffffff);
-      mForegroundColor = attributes.getColor(R.styleable.WindRoseDiagramView_foregroundColor, 0xffffffff);
-      mOvalBackgroundColor = attributes.getColor(R.styleable.WindRoseDiagramView_backgroundColor, 0xff00ff00);
-      mOutlineColor = attributes.getColor(R.styleable.WindRoseDiagramView_outlineColor, 0xffff5534);
-      mTextSize = attributes.getDimensionPixelOffset(R.styleable.WindRoseDiagramView_textSize, dp2px(12));
-      mOutlineWidth = attributes.getDimensionPixelOffset(R.styleable.WindRoseDiagramView_outlineWidth, dp2px(12));
+      mOvalBackgroundColor = attributes.getColor(R.styleable.WindRoseDiagramView_backgroundColor, 0);
+      mOutlineWidth = attributes.getDimensionPixelOffset(R.styleable.WindRoseDiagramView_outlineWidth, dp2px(1));
+      mOutlineColor = attributes.getColor(R.styleable.WindRoseDiagramView_outlineColor, 0xccffffff);
+      mTextColor = attributes.getColor(R.styleable.WindRoseDiagramView_textColor, 0xccffffff);
+      mTextSize = attributes.getDimensionPixelOffset(R.styleable.WindRoseDiagramView_textSize, dp2px(15));
+      mAnchorWidth = attributes.getDimensionPixelOffset(R.styleable.WindRoseDiagramView_anchorWidth, dp2px(4));
+      mAnchorColor = attributes.getColor(R.styleable.WindRoseDiagramView_anchorColor, 0xffffffff);
+      mForegroundColor = attributes.getColor(R.styleable.WindRoseDiagramView_foregroundColor, 0x881888ec);
+
+      mTouchSlop = attributes.getDimensionPixelOffset(R.styleable.WindRoseDiagramView_touchSlop, dp2px(24));
       mStartAngle = attributes.getFloat(R.styleable.WindRoseDiagramView_startAngle, 0);
     } finally {
       attributes.recycle();
     }
   }
 
-  private Paint mMarkPaint;
-
-  private void init() {
+  private void init(Context context) {
+    this.mCtx = context;
     setOnClickListener(onClickListener);
 
     mOvalPaint = new Paint();
     mOvalPaint.setAntiAlias(true);
-    mOvalPaint.setColor(mOvalBackgroundColor);
+    mOvalPaint.setStyle(Paint.Style.FILL);
+    mOvalBackgroundColor = 0;
 
     mOutlinePaint = new Paint();
     mOutlinePaint.setAntiAlias(true);
     mOutlinePaint.setStyle(Paint.Style.STROKE);
-    mOutlinePaint.setStrokeWidth(10);
-    mOutlinePaint.setColor(mOutlineColor);
+    mOutlineWidth = dp2px(1);
+    mOutlineColor = 0xccffffff;
 
     mTextPaint = new Paint();
     mTextPaint.setAntiAlias(true);
-    mTextPaint.setColor(mTextColor);
     mTextPaint.setStyle(Paint.Style.FILL);
+    mTextColor = 0xccffffff;
+    mTextSize = dp2px(15);
+
+    mAnchorPaint = new Paint();
+    mAnchorPaint.setAntiAlias(true);
+    mAnchorPaint.setStyle(Paint.Style.FILL);
+    mAnchorColor = 0xffffffff;
+    mAnchorWidth = dp2px(4);
+
+    mForegroundPaint = new Paint();
+    mForegroundPaint.setAntiAlias(true);
+    mForegroundColor = 0x881888ec;
+
+    mTouchSlop = dp2px(24);
+    mStartAngle = 0;
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+
+    // background
+    mOvalPaint.setColor(mOvalBackgroundColor);
+    canvas.drawOval(mOvalRectF, mOvalPaint);
+
+    // outline
+    mOutlinePaint.setColor(mOutlineColor);
+    mOutlinePaint.setStrokeWidth(mOutlineWidth);
+    canvas.drawOval(mOvalRectF, mOutlinePaint);
+
+
+    if (mAdapter == null) return;
+
+    mAnchorPaint.setColor(mAnchorColor);
+    mTextPaint.setColor(mTextColor);
     mTextPaint.setTextSize(mTextSize);
 
-    mMarkPaint = new Paint();
-    mMarkPaint.setAntiAlias(true);
-    mMarkPaint.setStyle(Paint.Style.STROKE);
-    mMarkPaint.setStrokeWidth(6);
-    mMarkPaint.setColor(0xff00ff00);
+    Path path = new Path();
+    path.moveTo(mForegroundDestPoints[0].x, mForegroundDestPoints[0].y);
+    for (int i = 0, count = mAdapter.getCount(); i < count; i++) {
+
+      canvas.drawOval(new RectF(mForegroundDestPoints[i].x-mAnchorWidth,
+          mForegroundDestPoints[i].y-mAnchorWidth,
+          mForegroundDestPoints[i].x+mAnchorWidth,
+          mForegroundDestPoints[i].y+mAnchorWidth), mAnchorPaint);
+
+      canvas.drawLine(mCenter.x, mCenter.y, mDestPoints[i].x, mDestPoints[i].y, mOutlinePaint);
+      canvas.drawLine(mForegroundDestPoints[i].x, mForegroundDestPoints[i].y,
+          mForegroundDestPoints[i + 1].x, mForegroundDestPoints[i + 1].y, mOutlinePaint);
+
+      mTextPaint.setTextAlign(Paint.Align.CENTER);
+      canvas.drawText(mAdapter.getName(i), mTextDestPoints[i].x, mTextDestPoints[i].y + (float) mTextSize / 2, mTextPaint);
+
+      if (i > 0) path.lineTo(mForegroundDestPoints[i].x, mForegroundDestPoints[i].y);
+    }
+
+    path.close();
+    mForegroundPaint.setColor(mForegroundColor);
+    canvas.drawPath(path, mForegroundPaint);
   }
 
   @Override
   protected void onSizeChanged(int w, int h, int oldw, int oldh) {
     super.onSizeChanged(w, h, oldw, oldh);
-
     updateOval();
-
     onDataSetChange();
   }
 
@@ -159,42 +214,6 @@ public class WindRoseDiagramView extends View {
     mRadius = mOvalRectF.width() / 2;
   }
 
-  @Override
-  protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
-
-    mOutlinePaint.setColor(mOutlineColor);
-    mOutlinePaint.setStrokeWidth(mOutlineWidth);
-    canvas.drawOval(mOvalRectF, mOutlinePaint);
-
-    mOvalPaint.setColor(mOvalBackgroundColor);
-    canvas.drawOval(mOvalRectF, mOvalPaint);
-
-    canvas.drawPoint(mOvalRectF.centerX(), mOvalRectF.centerY(), mOutlinePaint);
-
-    if (mAdapter == null) return;
-    for (int i = 0, count = mAdapter.getCount(); i < count; i++) {
-      canvas.drawPoint(mDestPoints[i].x, mDestPoints[i].y, mOutlinePaint);
-      canvas.drawPoint(mForegroundDestPoints[i].x, mForegroundDestPoints[i].y, mOutlinePaint);
-      canvas.drawLine(mCenter.x, mCenter.y, mDestPoints[i].x, mDestPoints[i].y, mOutlinePaint);
-      canvas.drawLine(mForegroundDestPoints[i].x, mForegroundDestPoints[i].y,
-          mForegroundDestPoints[i+1].x, mForegroundDestPoints[i+1].y, mOutlinePaint);
-
-      mTextPaint.setTextAlign(Paint.Align.CENTER);
-      canvas.drawText(mAdapter.getName(i), mTextDestPoints[i].x, mTextDestPoints[i].y+(float)mTextSize/2, mTextPaint);
-
-
-      float d = (float)dp2px(48) / 2;
-      RectF rectF = new RectF(mTextDestPoints[i].x-d, mTextDestPoints[i].y-d, mTextDestPoints[i].x+d, mTextDestPoints[i].y+d);
-      canvas.drawOval(rectF, mMarkPaint);
-      if (mLastTouchMotionEvent != null) {
-        int[] parentLocation = new int[2];
-        getLocationOnScreen(parentLocation);
-        canvas.drawPoint(mLastTouchMotionEvent.getRawX()-parentLocation[0], mLastTouchMotionEvent.getRawY()-parentLocation[1], mMarkPaint);
-      }
-    }
-  }
-
 
   public void onDataSetChange() {
     if (mAdapter == null || mCenter == null) return;
@@ -206,13 +225,13 @@ public class WindRoseDiagramView extends View {
     int realWidth = getWidth() - getPaddingLeft() - getPaddingRight();
     int realHeight = getHeight() - getPaddingTop() - getPaddingBottom();
     int diameter = realWidth > realHeight ? realHeight : realWidth;
-    float textRadius = (float)diameter/2 - (float)mTextSize;
+    float textRadius = (float) diameter / 2 - (float) mTextSize;
 
-    mForegroundDestPoints = new Point[count+1];
-    float angleFraction = (float) (2*Math.PI) / count;
+    mForegroundDestPoints = new Point[count + 1];
+    float angleFraction = (float) (2 * Math.PI) / count;
 
     for (int i = 0; i < count; i++) {
-      float angle = mStartAngle + i * angleFraction;
+      float angle = (float) (mStartAngle*Math.PI/180 + i * angleFraction);
       mDestPoints[i] = new Point((int) (mCenter.x + mRadius * Math.sin(angle)),
           (int) (mCenter.y - mRadius * Math.cos(angle)));
       mTextDestPoints[i] = new Point((int) (mCenter.x + textRadius * Math.sin(angle)),
@@ -237,18 +256,17 @@ public class WindRoseDiagramView extends View {
   private OnClickListener onClickListener = new OnClickListener() {
     @Override
     public void onClick(View v) {
-      if (mTextDestPoints == null || mLastTouchMotionEvent == null || windRoseClickListener == null) return;
-      int slop = dp2px(48);
+      if (mTextDestPoints == null || mLastTouchMotionEvent == null || windRoseClickListener == null)
+        return;
 
       int[] parentLocation = new int[2];
       getLocationOnScreen(parentLocation);
-      Point touchPoint = new Point((int) mLastTouchMotionEvent.getRawX()-parentLocation[0], (int) mLastTouchMotionEvent.getRawY()-parentLocation[1]);
+      Point touchPoint = new Point((int) mLastTouchMotionEvent.getRawX() - parentLocation[0],
+          (int) mLastTouchMotionEvent.getRawY() - parentLocation[1]);
 
-      System.out.println("#########  slop:"+slop);
       for (int i = 0, count = mTextDestPoints.length; i < count; i++) {
         double distance = getDistance(touchPoint, mTextDestPoints[i]);
-        System.out.println("####### i:"+i+"   distance:"+distance);
-        if (distance <= slop) {
+        if (distance <= mTouchSlop) {
           windRoseClickListener.onItemClick(i);
           break;
         }
@@ -257,7 +275,7 @@ public class WindRoseDiagramView extends View {
   };
 
   private double getDistance(Point p1, Point p2) {
-    return Math.floor((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
   }
 
   private int dp2px(float dp) {
@@ -274,4 +292,6 @@ public class WindRoseDiagramView extends View {
     this.mAdapter.setView(this);
     onDataSetChange();
   }
+
+
 }
